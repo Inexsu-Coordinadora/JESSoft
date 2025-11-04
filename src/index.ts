45480e35fd1a1eea7217778f262f7623f218fc25
saraamelc <saramellancano@gmail.com>
2025-11-04 17:28:12 -0500

    config final

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-import { startServer } from './presentation/app';
-import { config } from './common/configuracion';
-
+import { startServer } from './presentation/app.js';
+import { config } from './common/configuracion.js';
 async function main() {
   await startServer();
   console.log(`Servidor corriendo en http://localhost:${config.puerto}`);
@@ -11,8 +9,3 @@ main().catch((err) => {
   console.error('Error al iniciar el servidor:', err);
   process.exit(1);
 });
-=======
-import { startServer } from "./presentation/app.js";
-
-startServer();
->>>>>>> 57092ed83eb8caa141f2c1b9181df62e2a509a5b
