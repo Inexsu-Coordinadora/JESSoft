a7f270baaf30948173e0ee085578dd3feeddefd1
Sebastian Higuita <sebashuigui1268@gmail.com>
2025-11-04 21:48:12 -0500

    cambios en conexcion en postgres

--- src/core/infraestructura/postgres/ConexionPostgres.ts
+++ src/core/infraestructura/postgres/ConexionPostgres.ts
@@ -1,17 +1,13 @@
-import pkg from 'pg';
+import {Pool} from 'pg';
 import { configuracion } from '../../../common/configuracion';
+export const pool = new Pool(configuracion.db);
+pool.connect()
+  .then(() => console.log('Conectado a la base de datos:', configuracion.db.database))
+  .catch(err => console.error(' Error de conexión:', err.message));
 
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  host: configuracion.db.host,
-  port: configuracion.db.port,
-  user: configuracion.db.user,
-  password: configuracion.db.password,
-  database: configuracion.db.database,
-  ssl:
-    configuracion.db.ssl ||
-    {
-      rejectUnauthorized: false, // necesario para conexiones en la nube
-    },
-});
+export async function ejecutarConsulta(
+  consulta: string,
+  parametros?: Array<number | string>
+) {
+  return await pool.query(consulta, parametros);
+}
