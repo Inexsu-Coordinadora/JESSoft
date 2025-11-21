0a000b4841d30934bb2c666fb79db92af3326b37
7Edwin <7edwin.riverar@gmail.com>
2025-11-20 23:08:43 -0500

    Pruebas de integración de Docente y eliminacion de .js

--- src/presentation/app.ts
+++ src/presentation/app.ts
@@ -25,8 +25,8 @@ app.register(
 
 export const startServer = async (): Promise<void> => {
   try {
-    await app.listen({ port: Number(process.env.PUERTO) });
-    app.log.info("El servidor esta corriendo... en puerto " + process.env.PUERTO);
+    await app.listen({ port: Number(process.env.PORT) });
+    app.log.info("El servidor está corriendo en el puerto " + process.env.PORT);
   } catch (err) {
     app.log.error(`Error al ejecutar el servidor\n ${err}`);
 
